ddb0a04b52db0199b6cc1f12d43b9ac7cb0bfc87
David Cantú Delgado <42122802+DavidCantuDelgado@users.noreply.github.com>
2024-01-29 15:04:53 -0600

    step4

---
+++ tec3d-test/MovieRowView.swift
@@ -0,0 +1,18 @@
+//
+//  MovieRowView.swift
+//  tec3d-test
+//
+//  Created by David Cantú Delgado on 29/01/24.
+//
+
+import SwiftUI
+
+struct MovieRowView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MovieRowView()
+}
