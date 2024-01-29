aac5dda1f07f93e8064009496cc75cda58179143
David Cantú Delgado <42122802+DavidCantuDelgado@users.noreply.github.com>
2024-01-29 15:15:06 -0600

    step5

---
+++ tec3d-test/MovieDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  MovieDetailView.swift
+//  tec3d-test
+//
+//  Created by David Cantú Delgado on 29/01/24.
+//
+
+import SwiftUI
+
+struct MovieDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MovieDetailView()
+}
