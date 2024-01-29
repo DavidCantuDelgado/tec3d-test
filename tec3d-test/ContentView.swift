c29711f51649bc9057581fc47641e4af241c998d
David Cantú Delgado <42122802+DavidCantuDelgado@users.noreply.github.com>
2024-01-29 14:42:53 -0600

    Initial Commit

---
+++ tec3d-test/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  tec3d-test
+//
+//  Created by David Cantú Delgado on 29/01/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ tec3d-test/tec3d_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  tec3d_testApp.swift
+//  tec3d-test
+//
+//  Created by David Cantú Delgado on 29/01/24.
+//
+
+import SwiftUI
+
+@main
+struct tec3d_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
